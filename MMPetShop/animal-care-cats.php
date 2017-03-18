8fb386bc7af184d39f7ce596f366d428eae67d50
Del Manzano <verdelmanzano06@users.noreply.github.com>
2017-03-19 03:14:31 +0800

    Add files via upload

---
+++ MMPetShop/animal-care-cats.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>M&M - Animal Care: Cats</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="custom-css/custom.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color: #ECECEC;">
+			<nav class="navbar navbar-inverse lewis-header">
+		<div class="container">
+			<!--logo -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.php" class="navbar-left"><img src="media/header.png"></a>
+			</div>
+			<!--menu-items -->
+			<div class="collapse navbar-collapse" id="mainNavBar">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+				
+					<!--drop down menu -->
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Info <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#contact" data-toggle="modal">Contact</a></li>
+							<li><a href="#">Feedbacks</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Gallery</a></li>
+						</ul>
+					</li>
+
+					<li><a href="#">Shop</a></li>
+
+					<li><a href="#">Cart</a></li>
+					<!--drop down menu -->
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Account Page</a></li>
+							<li><a href="#">Logout</a></li>
+						</ul>
+					</li>
+				</ul>
+
+				<!--right align -->
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<form role="search" class="navbar-form col-xs-12">
+							<input type="search" class="form-control" placeholder="Search from shop"/>
+							<input type="button" class="btn btn-danger" value="Search"/>
+						</form>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10">
+				<div class="panel panel-default">
+					
+					<div class="panel-body">
+						<ol class="breadcrumb">
+							<li><a href="index.php">Home</a></li>
+							<li class="disabled">Animal Care</li>
+							<li class="active">Cats</li>						
+						</ol>
+
+						<div class="page-header">
+							<h2>Proper Caring of Cats</h2>
+						</div>
+					</div>
+					<img class="featuredImg" src="media/cats.jpg"/>
+					<p>Their long association with humans has led dogs to be uniquely attuned to human behavior[12] 
+					and they are able to thrive on a starch-rich diet that would be inadequate for other canid species.[13]
+					Dogs vary widely in shape, size and colours.[14] Dogs perform many roles for people, such as hunting, herding, 
+					pulling loads, protection, assisting police and military, companionship and, more recently, aiding handicapped 
+					individuals. This influence on human society has given them the sobriquet "man's best friend".</p>
+					<h4>Ten Caring Tips:</h4>
+					<p>Their long association with humans has led dogs to be uniquely attuned to human behavior[12] 
+					and they are able to thrive on a starch-rich diet that would be inadequate for other canid species.[13]
+					Dogs vary widely in shape, size and colours.[14] Dogs perform many roles for people, such as hunting, herding, 
+					pulling loads, protection, assisting police and military, companionship and, more recently, aiding handicapped 
+					individuals. This influence on human society has given them the sobriquet "man's best friend".</p>
+					<h4>Ten Caring Tips:</h4>
+					<p>Their long association with humans has led dogs to be uniquely attuned to human behavior[12] 
+					and they are able to thrive on a starch-rich diet that would be inadequate for other canid species.[13]
+					Dogs vary widely in shape, size and colours.[14] Dogs perform many roles for people, such as hunting, herding, 
+					pulling loads, protection, assisting police and military, companionship and, more recently, aiding handicapped 
+					individuals. This influence on human society has given them the sobriquet "man's best friend".</p>
+					<h4>Ten Caring Tips:</h4>
+					<p>Their long association with humans has led dogs to be uniquely attuned to human behavior[12] 
+					and they are able to thrive on a starch-rich diet that would be inadequate for other canid species.[13]
+					Dogs vary widely in shape, size and colours.[14] Dogs perform many roles for people, such as hunting, herding, 
+					pulling loads, protection, assisting police and military, companionship and, more recently, aiding handicapped 
+					individuals. This influence on human society has given them the sobriquet "man's best friend".</p>
+					<h4>Ten Caring Tips:</h4>
+					<p>Their long association with humans has led dogs to be uniquely attuned to human behavior[12] 
+					and they are able to thrive on a starch-rich diet that would be inadequate for other canid species.[13]
+					Dogs vary widely in shape, size and colours.[14] Dogs perform many roles for people, such as hunting, herding, 
+					pulling loads, protection, assisting police and military, companionship and, more recently, aiding handicapped 
+					individuals. This influence on human society has given them the sobriquet "man's best friend".</p>
+				</div>
+			</div>
+
+			<div class="col-lg-2">
+				<div class="list-group">
+					<h3><strong>Animal Care</strong></h3>
+					<a href="animal-care-dogs.php" class="list-group-item">
+						<h4 class="list-group-item-heading">Dogs</h4>
+					</a>
+					<a href="animal-care-cats.php" class="list-group-item active">
+						<h4 class="list-group-item-heading">Cats</h4>
+					</a>
+					<a href="animal-care-reptiles.php" class="list-group-item">
+						<h4 class="list-group-item-heading">Reptiles</h4>
+					</a>
+				</div>
+				<img src="media/ads.gif"/>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="navbar navbar-default navbar-static-bottom">
+		<div class="container">
+			<p class="navbar-text pull-left">&copy 2017, M&M Online PetShop</p>
+			<a class="navbar-btn btn btn-primary pull-right">Follow us on Facebook</a>
+		</div>
+	</div>
+
+	<div class="modal fade" id="contact" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form class="form-horizontal">
+				<div class="modal-header">
+					<h4>Contact our Store</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label for="contact-name" class="col-lg-2 control-label">Name:</label>
+						<div class="col-lg-10">
+							<input type="text" class="form-control" id="contact-name" placeholder="Full Name">
+						</div>
+
+					</div>
+					<div class="form-group">
+						<label for="contact-email" class="col-lg-2 control-label">Email:</label>
+						<div class="col-lg-10">
+							<input type="email" class="form-control" id="contact-email" placeholder="you@example.com">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="contact-msg" class="col-lg-2 control-label">Message:</label>
+						<div class="col-lg-10">
+							<textarea class="form-control" row="8" placeholder="Your Message" id="contact-msg"></textarea>
+						</div>
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary" data-dismiss="modal">Send</button>
+					<button type="button" class="btn btn-info" data-dismiss="modal">Cancel</button>
+				</div>
+			</form>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
